61b929318c8a1f2a21d2e7595bbb697d66bd4283
Valdir Viana <viana.valdir@gmail.com>
2018-09-20 21:13:28 -0300

    initial app

---
+++ Mediatr.Console/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Mediatr.ConsoleApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var serviceProvider = new ServiceCollection()
+                .AddLogging()
+                .BuildServiceProvider();
+
+            serviceProvider
+                .GetService<ILoggerFactory>()
+                .AddConsole(LogLevel.Debug);
+
+
+            var logger = serviceProvider.GetService<ILoggerFactory>()
+                .CreateLogger<Program>();
+            logger.LogDebug("Starting application");
+
+
+            logger.LogDebug("All done!");
+
+            Console.ReadKey();
+        }
+    }
+}
