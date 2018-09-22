fb4cb5e661fee42d9cd6f67a846537bda4b391c7
Valdir Viana <viana.valdir@gmail.com>
2018-09-22 15:58:01 -0300

    Insert mediatr

--- Mediatr.Console/Program.cs
+++ Mediatr.Console/Program.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Reflection;
 
 namespace Mediatr.ConsoleApp
 {
@@ -10,21 +12,32 @@ namespace Mediatr.ConsoleApp
         {
             Console.WriteLine("Hello World!");
 
-            var serviceProvider = new ServiceCollection()
+            IServiceCollection services = new ServiceCollection();
+            
+            var provider = services
                 .AddLogging()
                 .BuildServiceProvider();
 
-            serviceProvider
+            //serviceProvider.AddScoped(typeof(IPipelineBehavior<,>), typeof(Timer<,>));
+            //serviceProvider.AddScoped(typeof(IPipelineBehavior<,>), typeof(Logging<,>));
+            //serviceProvider.AddScoped(typeof(IPipelineBehavior<,>), typeof(Validator<,>));
+            services.AddMediatR(typeof(Program).GetTypeInfo().Assembly);
+
+            provider
                 .GetService<ILoggerFactory>()
                 .AddConsole(LogLevel.Debug);
 
-
-            var logger = serviceProvider.GetService<ILoggerFactory>()
+            var logger = provider.GetService<ILoggerFactory>()
                 .CreateLogger<Program>();
             logger.LogDebug("Starting application");
 
 
             logger.LogDebug("All done!");
+
+            foreach (var service in services)
+            {
+                Console.WriteLine(service.ServiceType + " - " + service.ImplementationType);
+            }
 
             Console.ReadKey();
         }
